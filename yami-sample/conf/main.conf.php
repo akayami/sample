0847d783e48273970b2c1525866e0e05c2841718
Tomasz Rakowski <akayami@gmail.com>
2012-04-23 18:15:54 -0400

    Setting up a small sample app

--- yami-sample/conf/main.conf.php
+++ yami-sample/conf/main.conf.php
@@ -1,1 +1,53 @@
 <?php
+/*
+ * Storage Config
+ */
+
+
+$config['db'] = array( 
+	'default' => array ( 
+		'master'  => array(
+			'servers' => array(
+				array(
+					'hostname' => 'localhost'
+				)
+			),
+			'shared' => array(
+				'socket' => '/var/run/mysqld/mysqld.sock',
+				'username' => 'root', 
+				'password' => '',
+				'dbname' => 'bookstore',
+				'persistent' => false,
+				'adapter' => 'yami\Database\Adapter\PDO'
+			)
+		),
+		'slave' => array(
+			'servers' => array(
+				array(
+					'hostname' => 'localhost'
+				)
+			),
+			'shared' => array(
+				'socket' => '/var/run/mysqld/mysqld.sock',
+				'username' => 'readonly', 
+				'password' => '',
+				'dbname' => 'bookstore',
+				'persistent' => false,
+				'adapter' => 'yami\Database\Adapter\PDO'
+			)
+		)
+	)
+);
+
+/*
+ * Memcached Config
+ */
+$config['mc'] = array(
+	'default' => array(
+		array('127.0.0.1', 11211)
+	)
+);
+
+
+
+
---
+++ yami-sample/controllers/yamiSample/Author.php
@@ -0,0 +1,14 @@
+<?php
+namespace yamiSample;
+use yami\Router\Action\Controller;
+
+class Author extends Controller {
+	
+	protected static $tableName = 'Author';
+	protected static $ids = array('id');
+	protected static $backendConfig = array(
+			array('\yami\Mc\Manager' => 'default'),
+			array('\yami\Database\Manager' => 'default')
+	);
+	
+} 
