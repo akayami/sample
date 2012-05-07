3a32e0426e49a7f07a63498a0e8d98f992ee23e2
Tomasz Rakowski <akayami@gmail.com>
2012-05-07 16:57:20 -0400

    Test file updated.

--- yami-sample/www/parser-test.php
+++ yami-sample/www/parser-test.php
@@ -7,6 +7,8 @@
 
 // use yami\Database\Sql\Table;
 
+use yami\Database\Sql2\Condition;
+
 use yami\Database\Sql2\Select;
 
 use yami\Redis\Manager;
@@ -19,21 +21,41 @@ require_once('autoload.inc.php');
 require_once('main.conf.php');
 @include('local.conf.php');
 
+
+// $parser = new PHPSQLParser("GROUP BY a, b, table.c");
+// print_r($parser->parsed);
+// //exit;
+
+// $parser = new PHPSQLParser("ORDER BY a ASC, b DESC, table.c ASC");
+// print_r($parser->parsed);
+// exit;
+
+
+//$q2 = new Select();
+//$q2->order('p desc')->group('p');
+//$q2->field('*')->table('table')->where('id=5 and cock like \'large\' and (a=5 and b=6)')->where('b=5')->limit('20 OFFSET 10')->group('p')->having('p > 5')->order('p DESC');
+//echo $q2;
+//exit;
+
 //$q = "SELECT `table`.`field` as a FROM Home JOIN Something USING id WHERE id = (SELECT user_id from users where email like '%cock?cock.com%') LIMIT 10 OFFSET 20 ORDER BY id";
-// $q = 'SELECT `field1` as a, field2 as b, field3 FROM `home` as a WHERE field1 LIKE \'{int:test}\' AND (field2 LIKE \'%cock%\' OR crap=1)';
-// $q = 'SELECT `field1` as a, field2 as b, field3 FROM `home` as a WHERE field1 LIKE \'{int:test}\' AND (field2 LIKE \'%cock%\' OR crap=('.$q.')';
+$q = 'SELECT `field1` as a, field2 as b, field3 FROM `home` as a WHERE field1 LIKE \'{int:test}\' AND (field2 LIKE \'%cock%\' OR crap=1)';
+$q = 'SELECT `field1` as a, field2 as b, field3 FROM `home` as a WHERE field1 LIKE \'{int:test}\' AND (field2 LIKE \'%cock%\' OR crap=('.$q.')';
 
 //$q = 'SELECT * FROM table WHERE a=1 ORDER BY c DESC LIMIT 10 OFFSET 20';
 //$q = 'SELECT * FROM table WHERE a=1 ORDER BY c DESC, RAND() LIMIT 20, 10';
-$q = 'SELECT schema.`table`.c as b, sum(id + 5 * (5 + 5)) as p FROM schema.table WHERE a=1 GROUP BY c HAVING p > 10 ORDER BY p DESC';
+//$q = 'SELECT schema.`table`.c as b, sum(id + 5 * (5 + 5)) as p FROM schema.table JOIN table.x as aliasx ON a=b LEFT JOIN table.c USING joincolumn JOIN table.d JOIN (SELECT * FROM someOTHERTable) as ot WHERE a=1 GROUP BY c HAVING p > 10 ORDER BY p DESC LIMIT 10 OFFSET 20';
+//$q = 'SELECT schema.`table`.c as b, sum(id + 5 * (5 + 5)) as p FROM schema.table JOIN table.x as aliasx ON a=b LEFT JOIN table.c USING joincolumn JOIN table.d JOIN (SELECT * FROM someOTHERTable) as ot WHERE a=1 GROUP BY c, `table`.a  HAVING p > 10 ORDER BY p DESC, `table`.a asc LIMIT 10,20';
 //$parser = new PHPSQLParser($q);
 // print_r($parser->parsed);
 // exit;
 
-$parser = new PHPSQLParser($q);
-print_r($parser->parsed);
-exit;
+// $parser = new PHPSQLParser($q);
+// print_r($parser->parsed);
+// exit;
+$q = 'SELECT * FROM t1 UNION ALL SELECT * FROM t2 UNION ALL SELECT * FROM t3 UNION ALL SELECT field1, field2 FROM t4';		
+
 $q = new Select($q);
+//print_r($q);
 //print_r($q);
 echo $q;
 exit;
